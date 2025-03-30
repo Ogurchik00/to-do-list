ffa0877e42655d2e6a445400a88f00bac2897ac9
Ogurchik00 <nik.skripka.000@gmail.com>
2025-03-30 23:02:08 +0900

    added basic functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,41 @@
-console.log("Hello")
+const taskForm = document.querySelector('.task-form')
+const taskInput = document.querySelector('.task-add-text')
+const taskList = document.querySelector('.tasks-list')
+
+taskForm.addEventListener('submit', (event) => {
+	event.preventDefault()
+
+	const taskText = taskInput.value
+
+	const taskHTML = `
+		<li class="task">
+			<span class="task-text">
+				${taskText}
+			</span>
+			<button data-action="done" class="task-complete-button">
+				Выполнена
+			</button>
+			<button data-action="delete" class="task-delete-button">
+				Удалить
+			</button>
+		</li>
+	`
+	taskList.insertAdjacentHTML('beforeend', taskHTML)
+
+	taskInput.value = ''
+	taskInput.focus()
+})
+
+taskList.addEventListener('click', (event) => {
+	if(event.target.dataset.action === 'delete') {
+		const parenNode = event.target.closest('.task')
+		parenNode.remove()
+	}
+})
+
+taskList.addEventListener('click', (event) => {
+	if(event.target.dataset.action === 'done') {
+		const parenNode = event.target.closest('.task')
+		parenNode.classList.toggle('task--completed')
+	}
+})
